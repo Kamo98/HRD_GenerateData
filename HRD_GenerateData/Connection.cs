1af91ee7a5fd72a7d51b88e38f9b9f731bc64bf6
Kamo98 <avanesjan.k@gmail.com>
2020-03-02 12:08:25 +0700

    Добавьте файлы проекта.

---
+++ HRD_GenerateData/Connection.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRD_GenerateData
+{
+	class Connection
+	{
+		public Connection ()
+		{
+
+		}
+
+		private void connect_to_db()
+		{
+
+		}
+	}
+}
